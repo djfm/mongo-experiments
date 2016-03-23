9fe28dbf981dfcef15db1f08ed27086beea6dd69
djfm <fm.de.jouvencel@gmail.com>
2016-03-23 11:08:57 +0100

    Improved example documentation

--- prices.js
+++ prices.js
@@ -1,14 +1,48 @@
 /* eslint-disable no-console */
 const MongoClient = require('mongodb').MongoClient;
+const expect = require('chai').expect;
 
 const url = 'mongodb://localhost:27017/shopgun-experiments-product';
 const PRICES_COL = 'prices';
 
-const runPricesExperiment = db => {
+const runPricesExperiment = pricesCollection => {
   console.log('Running price experiment...');
 
+  /**
+   * We're given a list of price rules,
+   * with prices that are overriden for some customer
+   * regions.
+   * A "*" means that the rule matches all regions.
+   */
+  const priceRules = [
+    { productId: 'A', region: '*', price: 10 },
+    { productId: 'A', region: 'FR', price: 20 },
+    { productId: 'B', region: '*', price: 15 },
+  ];
+
+  /**
+   * We want to get the prices for all products
+   * for French customers, knowing that if several
+   * price rules match for a product, we want to
+   * take the most specific one into account.
+   */
+  const priceQuery = { region: 'FR' };
+
+  /**
+   * We expect the resulting price list to be the following:
+   */
+  const expectedPriceList = [
+    { productId: 'A', price: 20 },
+    { productId: 'B', price: 15 },
+  ];
+
+  /**
+   * The next part sets the database up and performs
+   * a mapReduce to find the correct price list.
+   */
+
   function map() {
-    /* global emit, query */
+   /* global emit, query */
     const acceptRecord = function acceptRecord(specificity) {
       /* eslint-disable object-shorthand */
       emit(this.productId, { price: this.price, specificity: specificity });
@@ -28,16 +62,25 @@ const runPricesExperiment = db => {
     })[0];
   }
 
-  const prices = db.collection(PRICES_COL);
-  return prices.insertMany([
-    { productId: 'A', region: '*', price: 10 },
-    { productId: 'A', region: 'FR', price: 20 },
-    { productId: 'B', region: '*', price: 15 },
-  ]).then(() => {
-    const query = { region: 'FR' };
-    const options = { out: { inline: 1 }, scope: { query } };
-    return prices.mapReduce(map, reduce, options);
-  }).then(console.log);
+  function finalize(key, reducedValue) {
+    return {
+      productId: key,
+      price: reducedValue.price,
+    };
+  }
+
+  return pricesCollection.insertMany(priceRules).then(() => {
+    const options = { out: { inline: 1 }, scope: { query: priceQuery }, finalize };
+    return pricesCollection.mapReduce(map, reduce, options);
+  }).then(
+    results => results.map(result => result.value)
+  ).then(
+    priceList => {
+      expect(priceList).to.deep.equal(expectedPriceList);
+      console.log('Successfully computed price list!');
+      console.log(priceList);
+    }
+  );
 };
 
 MongoClient.connect(url)
@@ -47,7 +90,7 @@ MongoClient.connect(url)
       return db.collection(PRICES_COL).remove().then(
         () => console.log('Removed collection %s.', PRICES_COL)
       ).then(
-        () => runPricesExperiment(db)
+        () => runPricesExperiment(db.collection(PRICES_COL))
       ).then(
         () => db.close()
       ).catch(err => {
